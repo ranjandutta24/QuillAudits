124456a0a01856d647e18db76631c1ba7fb5f436
Ranjan Dutta <connecttoranjan@gmail.com>
2022-12-02 10:50:35 +0530

    slider done

--- js/main.js
+++ js/main.js
@@ -20,6 +20,24 @@ const slider = function () {
   const maxSlide = slides.length;
 
   // Functions
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots__dot" data-slide="${i}"></button>`
+      );
+    });
+  };
+
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll(".dots__dot")
+      .forEach(dot => dot.classList.remove("dots__dot--active"));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add("dots__dot--active");
+  };
 
   const goToSlide = function (slide) {
     slides.forEach(
@@ -36,6 +54,7 @@ const slider = function () {
     }
 
     goToSlide(curSlide);
+    activateDot(curSlide);
   };
 
   const prevSlide = function () {
@@ -45,7 +64,16 @@ const slider = function () {
       curSlide--;
     }
     goToSlide(curSlide);
+    activateDot(curSlide);
   };
+
+  const init = function () {
+    goToSlide(0);
+    createDots();
+
+    activateDot(0);
+  };
+  init();
 
   // Event handlers
   btnRight.addEventListener("click", nextSlide);
@@ -54,6 +82,14 @@ const slider = function () {
   document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowLeft") prevSlide();
     e.key === "ArrowRight" && nextSlide();
+  });
+
+  dotContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots__dot")) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateDot(slide);
+    }
   });
 };
 slider();
